f2c33cfe17e9835ecefa2a5e1e199c29d9ab94e7
moniajod <65946948+moniajod@users.noreply.github.com>
2020-05-26 10:42:28 +0200

    Piąty commit

--- 01program.php
+++ 01program.php
@@ -4,4 +4,5 @@ echo ("<br> Jestem programem 01");
 echo ("<br> Zadanie samodzielne! :)");
 echo ("<br> Informatyka i technologie internetowe");
 echo ("<br> Dalsza czesc programu");
+echo ("<br> Dalsza czesc programu z GitHuba");
 ?>
